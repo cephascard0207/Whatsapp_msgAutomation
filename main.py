9b23fbb44f4e49dc51ddc997846041d75b42bda0
Cephas Cardozo <cephascardozo0207@gmail.com>
2022-05-14 13:02:39 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Whatsapp_msgAutomation
+# Created by Cephas Cardozo
+# Developed using Python
+
+# imports
+import pywhatkit
+
+# send_whatsappMsg
+pywhatkit.sendwhatmsg("Put your country code with your phone number here", "Your message goes here", 13, 00)
+# end_ofCode
